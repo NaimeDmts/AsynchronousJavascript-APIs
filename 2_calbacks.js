fce20df741775505dd2902d87f7d73a6fbeb3c69
NaimeDmts <naime.dmts@gmail.com>
2023-03-21 10:55:13 +0300

    7- Promise

--- 2_calbacks.js
+++ 2_calbacks.js
@@ -34,3 +34,48 @@ const getPostDetails = (post, callback) =>{
 //callback hell
 
 console.log("end");
+
+
+// console.log("start");
+
+// const serverStatus = false;
+// const login = (username, password, successFn, errorFn) =>{
+//     setTimeout(() =>{
+//         if(serverStatus){
+//             successFn({username:username, email:"naime.dmts@gmail.com"})
+//         }
+//         else{
+//             errorFn("Server kapalı");
+//         }
+        
+//     },1000);
+// }
+
+// const getPostsByUsername = (username, successFn) => {
+//     setTimeout(()=>{
+//         successFn(["post 1", "post 2","post 3"])
+//     },500);
+// }
+
+// const getPostDetails = (post, successFn) =>{
+//     setTimeout(()=>{
+//         successFn("Post Details");
+//     },500)
+// }
+
+//  login("Naime", "123", user =>{
+//     console.log(user.username);
+//     console.log(user.email);
+
+//     getPostsByUsername(user.username, (posts) => {
+//         console.log(posts);
+
+//         getPostDetails(posts[0], (details)=>{
+//             console.log(details);
+//         })
+//     });
+
+// },(error)=>{
+//     console.log(error);
+// });
+// console.log("end");
