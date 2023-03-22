8289c1902412c042e4326959aabeeb670da7c9aa
NaimeDmts <naime.dmts@gmail.com>
2023-03-22 15:55:43 +0300

    13- Konuma Göre Ülke Bilgisi - Location Api

--- GeoLocation/geoLocation.js
+++ GeoLocation/geoLocation.js
@@ -6,8 +6,17 @@ document.getElementById('btnLocation').addEventListener('click',()=>{
 function onError(err){
     console.log(err);
 }
-function onSuccess(position){
+async function onSuccess(position){
     let lat = position.coords.latitude;
     let lng = position.coords.longitude;
-    console.log(lat , lng)
+   
+    const api_key = "681336af8d6e473899858540079e557b";
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${api_key}`;
+
+    const response = await fetch(url)
+    const data = await response.json();
+    const country = data.results[0].components.country;
+    
+    document.querySelector('#txtSearch').value = country;
+    document.querySelector('#btnSearch').click();
 }
